cd59ca838855ef3af98af7664ec6f6e794a1ffa3
INKWWW <whmink@foxmail.com>
2018-11-29 14:11:48 +0800

    update dict_test

--- utils/dict_test.py
+++ utils/dict_test.py
@@ -12,8 +12,46 @@ def dictDel():
     # print('delete this dict: ', dd) #
     print('delete this dict')
 
+
+class A(object): 
+    def __init__(self): 
+        self['B'] = "BB" 
+        self['D'] = "DD" 
+        del self['D'] 
+        # print('__init__: {}'.format(self.__init__))
+       
+    def __setitem__(self,name,value): 
+        '''''
+        @summary: 每当属性被赋值的时候都会调用该方法，因此不能再该方法内赋值 self.name = value 会死循环
+        ''' 
+        print("__setitem__:Set {} Value {}".format(name,value))
+        self.__dict__[name] = value 
+       
+    def __getitem__(self,name): 
+        ''''' 
+        @summary: 当访问不存在的属性时会调用该方法
+        ''' 
+        print("__getitem__:No attribute named '{}'".format(name))
+        return None 
+       
+    def __delitem__(self,name): 
+        ''''' 
+        @summary: 当删除属性时调用该方法
+        ''' 
+        print("__delitem__:Delect attribute '{}'".format(name))
+        del self.__dict__[name] 
+        print(self.__dict__) 
+
+
 if __name__ == '__main__':
-    dictDel()
+    # dictDel()
+
+    print('### __init__ process: ###')
+    X = A() 
+    print('### check bb in this dict ? ###')
+    b = X['bb'] 
+    print('### add key:value to this dict: ')
+    X['cc'] = 'CC'
 
 
 
