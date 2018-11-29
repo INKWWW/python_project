988eba9ddf06f22381047358332521fcaeb1e87b
INKWWW <whmink@foxmail.com>
2018-11-29 20:40:45 +0800

    upload

---
+++ utils/star_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def star_traverse_set():
+    test_set = ((1, 2), (2, 3), (3, 4), (4, 5))
+    for item in test_set:
+        print('*item_set: {}'.format(*item))
+
+
+def star_traverse_list():
+    test_list = [(1, 2), (2, 3), (3, 4), (4, 5)]
+    # for item in test_list:
+    print('*item_list: {}'.format(*test_list))
+
+
+
+if __name__ == '__main__':
+    print('### test star set ###')
+    star_traverse_set()
+    print('### test star list ###')
+    star_traverse_list()
+
+
