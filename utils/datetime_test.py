f2713948c3c533e08a9eec85b0b4d4d595377d9c
INKWWW <whmink@foxmail.com>
2019-01-04 16:57:51 +0800

    upload datetime_test script

--- utils/datetime_test.py
+++ utils/datetime_test.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import datetime
+from datetime import datetime 
+from datetime import timedelta
+import time
 
 def timeGap(date1, date2):
-    time1 = datetime.datetime(int(date1[0:4]), int(date1[4:6]), int(date1[6:8]))
-    time2 = datetime.datetime(int(date2[0:4]), int(date2[4:6]), int(date2[6:8]))
+    '''calculate the gap between two time'''
+    time1 = datetime(int(date1[0:4]), int(date1[4:6]), int(date1[6:8]), int(date1[8:10]))
+    time2 = datetime(int(date2[0:4]), int(date2[4:6]), int(date2[6:8]))
     print('time1 is ', time1)
     print('time2 is ', time2)
     gap = (time1 - time2).days
@@ -13,8 +16,39 @@ def timeGap(date1, date2):
     return gap
 
 
+def tomorrow():
+    '''get the detailed date of tomorrow'''
+    now = datetime.now()
+    print("now: ", now)
+    aDay = timedelta(days=1)  # -1: yesterday
+    now = now + aDay
+    print("tomorrow: ", now.strftime('%Y-%m-%d'))
+
+
+def nextYear():
+    now = datetime.now()
+    next_year = now + timedelta(days=365)
+    print('next year is', next_year)
+
+
+def lastMonth():
+    '''find last month'''
+    now_struct_time = time.localtime()
+    print('now_struct_time: ', now_struct_time)
+    last_month = now_struct_time[1] - 1 or 12
+    print('last month is ', last_month)
+
 
 if __name__ == '__main__':
     date1 = '20181008152339'
     date2 = '20181001104600'
     timeGap(date1, date2)
+
+    #####
+    # tomorrow()
+
+    #####
+    # nextYear()
+
+    #####
+    # lastMonth()
